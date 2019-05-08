770a1baab8bf3bf8a061354d649da3bc7cc4d79d
sachitha2 <shpsachitha@gmail.com>
2019-05-08 12:11:45 +0530

    Net checking, Loading adding

--- app/src/main/java/com/example/chata/projectcmi/Settings.java
+++ app/src/main/java/com/example/chata/projectcmi/Settings.java
@@ -39,28 +39,44 @@ public class Settings extends AppCompatActivity {
     }
 
     public int createDatabases(){
-        sqlite = openOrCreateDatabase("infini", Context.MODE_PRIVATE,null);
+        sqlite = openOrCreateDatabase("cmi", Context.MODE_PRIVATE,null);
+
+        ////Creating Area Database
+
+        sqlite.execSQL("CREATE TABLE IF  NOT EXISTS area (id int(11) NOT NULL,name varchar(200) NOT NULL);");
+
+        ///Creating Customer Database
+        sqlite.execSQL("CREATE TABLE IF  NOT EXISTS customer (" +
+                "id int(11) NOT NULL," +
+                "  name varchar(200) NOT NULL," +
+                "  address text NOT NULL," +
+                "  tp varchar(10) NOT NULL," +
+                "  regdate date NOT NULL," +
+                "  areaid int(3) NOT NULL," +
+                "  nic varchar(15) NOT NULL," +
+                "  agentid int(3) NOT NULL," +
+                "  status int(1) NOT NULL );");//COMMENT '1=active,0=not active'
 
 
-        sqlite.execSQL("CREATE TABLE IF  NOT EXISTS test (ROLL_NO int(3),NAME varchar(20));");
-
-        sqlite.execSQL("INSERT INTO test (ROLL_NO, NAME) VALUES ('11', '77');");
-        sqlite.execSQL("INSERT INTO test (ROLL_NO, NAME) VALUES ('1', '7');");
 
 
-        Cursor c =sqlite.rawQuery("SELECT * FROM test;",null);
+        //sqlite.execSQL("INSERT INTO test (ROLL_NO, NAME) VALUES ('11', '77');");
+        //sqlite.execSQL("INSERT INTO test (ROLL_NO, NAME) VALUES ('1', '7');");
 
-        if(c.getCount() == 0 ){
-            Toast.makeText(this, "No data in database", Toast.LENGTH_SHORT).show();
-        }
-        int nRow = c.getCount();
-        Toast.makeText(this,Integer.toString(nRow), Toast.LENGTH_SHORT).show();
 
-        StringBuffer buffer = new StringBuffer();
-        while (c.moveToNext()){
-            buffer.append("NAME "+c.getString(1));
-        }
-        Toast.makeText(this,buffer.toString(), Toast.LENGTH_SHORT).show();
+        //Cursor c =sqlite.rawQuery("SELECT * FROM test;",null);
+
+//        if(c.getCount() == 0 ){
+//            Toast.makeText(this, "No data in database", Toast.LENGTH_SHORT).show();
+//        }
+//        int nRow = c.getCount();
+//        Toast.makeText(this,Integer.toString(nRow), Toast.LENGTH_SHORT).show();
+//
+//        StringBuffer buffer = new StringBuffer();
+//        while (c.moveToNext()){
+//            buffer.append("NAME "+c.getString(1));
+//        }
+//        Toast.makeText(this,buffer.toString(), Toast.LENGTH_SHORT).show();
 
         return 1;
     }
