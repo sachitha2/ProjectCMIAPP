ffefc5bd21f279bd404d70303255f395a1f904d7
sachitha2 <shpsachitha@gmail.com>
2019-05-21 11:03:33 +0530

    Creating list with Table Data

--- app/src/main/java/com/example/chata/projectcmi/SelectAreaForCredits.java
+++ app/src/main/java/com/example/chata/projectcmi/SelectAreaForCredits.java
@@ -21,35 +21,36 @@ public class SelectAreaForCredits extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_area_for_credits);
-//        sqlite = openOrCreateDatabase("cmi", Context.MODE_PRIVATE,null);//10
+        sqlite = openOrCreateDatabase("cmi", Context.MODE_PRIVATE,null);//10
 
-        name = new String[5];
-        id = new String[5];
+
         areaList = findViewById(R.id.areaList);
 
-//        Cursor c =sqlite.rawQuery("SELECT * FROM area;",null);
+        Cursor c =sqlite.rawQuery("SELECT * FROM area;",null);
 
-//        if(c.getCount() == 0 ){
-//            Toast.makeText(this, "No data in database", Toast.LENGTH_SHORT).show();
-//        }
-//        int nRow = c.getCount();
-//        Toast.makeText(this,Integer.toString(nRow), Toast.LENGTH_SHORT).show();
-//
-//        StringBuffer buffer = new StringBuffer();
-//        int i = 0;
-//        while (c.moveToNext()){
-//            buffer.append("NAME "+c.getString(1));
-//            name[i] = "sachitha";
-//            id[i] = "sam";
-//            i++;
-//        }
-//
+        if(c.getCount() == 0 ){
+            Toast.makeText(this, "No data in database", Toast.LENGTH_SHORT).show();
+        }else{
+            name = new String[c.getCount()];
+            id = new String[c.getCount()];
 
 
-        name[0] = "sachitha";
-        id[0] = "sam";
-                           // then create the grid view
-                            ListViewAdapter areaListViewAdapter = new ListViewAdapter(this, name, id,"SelectArea");
-                            areaList.setAdapter(areaListViewAdapter);
+
+            int nRow = c.getCount();
+            Toast.makeText(this,Integer.toString(nRow), Toast.LENGTH_SHORT).show();
+
+            StringBuffer buffer = new StringBuffer();
+            int i = 0;
+            while (c.moveToNext()){
+                buffer.append("NAME "+c.getString(1));
+                name[i] = c.getString(1);
+                id[i] = c.getString(0);
+                i++;
+            }
+            // then create the grid view
+            ListViewAdapter areaListViewAdapter = new ListViewAdapter(this, name, id,"SelectArea");
+            areaList.setAdapter(areaListViewAdapter);
+        }
+
     }
 }
