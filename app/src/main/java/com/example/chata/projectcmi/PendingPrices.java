3870a99516abd1f78085171a938187b98c9d6ace
sachitha2 <shpsachitha@gmail.com>
2019-07-25 20:44:40 +0530

    Creat pending prices Activity

---
+++ app/src/main/java/com/example/chata/projectcmi/PendingPrices.java
@@ -0,0 +1,13 @@
+package com.example.chata.projectcmi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PendingPrices extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pending_prices);
+    }
+}
