c05201050634ed15bece91fc6df88f89523e07d1
sachitha2 <shpsachitha@gmail.com>
2019-11-15 12:57:20 +0530

    Created Single row for payment history

--- app/src/main/java/com/example/chata/projectcmi/SingleRowForPaymentHistory.java
+++ app/src/main/java/com/example/chata/projectcmi/SingleRowForPaymentHistory.java
@@ -1,4 +1,45 @@
 package com.example.chata.projectcmi;
 
 public class SingleRowForPaymentHistory {
+
+    int id;
+    float collection;
+    String date;
+
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public float getCollection() {
+        return collection;
+    }
+
+    public void setCollection(float collection) {
+        this.collection = collection;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+
+
+    public SingleRowForPaymentHistory(int id, float collection, String date) {
+        this.id = id;
+        this.collection = collection;
+        this.date = date;
+    }
+
+
+
 }
