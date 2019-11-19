12402f69b7764b64b9373510ee67cab8963870d5
sachitha2 <shpsachitha@gmail.com>
2019-11-19 20:20:36 +0530

    created installment view

--- app/src/main/java/com/example/chata/projectcmi/InstallmentsHead.java
+++ app/src/main/java/com/example/chata/projectcmi/InstallmentsHead.java
@@ -1,15 +1,30 @@
 package com.example.chata.projectcmi;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class InstallmentsHead extends AppCompatActivity {
-
+    Button all,month,week,today,passed;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_installments_head);
 
+        all = findViewById(R.id.btnAll);
+
+        all.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentInstall = new Intent(InstallmentsHead.this,ViewInstallments.class);
+                startActivity(intentInstall);
+            }
+        });
+
         setTitle("Installments");
+
+
     }
 }
---
+++ app/src/main/java/com/example/chata/projectcmi/ViewInstallments.java
@@ -0,0 +1,14 @@
+package com.example.chata.projectcmi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ViewInstallments extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_installments);
+        setTitle("View Installments");
+    }
+}
