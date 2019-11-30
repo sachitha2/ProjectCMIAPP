34f7d51c95bf8d75a1b11693fc1737b34da569ac
sachitha2 <shpsachitha@gmail.com>
2019-11-30 09:33:41 +0530

    Added bill print part to Add payment activity states working

--- app/src/main/java/com/example/chata/projectcmi/CustomersInstallment.java
+++ app/src/main/java/com/example/chata/projectcmi/CustomersInstallment.java
@@ -1,9 +1,14 @@
 package com.example.chata.projectcmi;
 
+import android.app.ProgressDialog;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.os.Handler;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -14,9 +19,49 @@ import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.ArrayList;
+import java.util.Set;
+import java.util.UUID;
 
 public class CustomersInstallment extends AppCompatActivity  {
+
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    //For bluetooth
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    // will show the statuses like bluetooth open, close or data sent
+    TextView myLabel;
+
+    // will enable user to enter any text to be printed
+    EditText myTextbox;
+
+    // android built in classes for bluetooth operations
+    BluetoothAdapter mBluetoothAdapter;
+    BluetoothSocket mmSocket;
+    BluetoothDevice mmDevice;
+
+    // needed for communication to bluetooth device / network
+    OutputStream mmOutputStream;
+    InputStream mmInputStream;
+    Thread workerThread;
+
+    byte[] readBuffer;
+    int readBufferPosition;
+    volatile boolean stopWorker;
+    String  BILL;
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    //For bluetooth
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
     private String InvoiceId;
 
 
@@ -135,7 +180,27 @@ public class CustomersInstallment extends AppCompatActivity  {
                         alert.show();
                     }
 
-                    updateStatus(InvoiceId);
+
+
+
+
+                        final ProgressDialog progressDialog = new ProgressDialog(CustomersInstallment.this);
+                        progressDialog.setTitle(" Printing Bill");
+                        progressDialog.setMessage("");
+                        progressDialog.setCancelable(false);
+                        updateStatus(InvoiceId);
+                        //bill print here
+                        try {
+
+                            findBT();
+                            openBT();
+                            progressDialog.show();
+                            BILL = "HELLOOO \n hii \n SAM";
+                            sendData(BILL,progressDialog);
+                        } catch (IOException ex) {
+                            ex.printStackTrace();
+                        }
+
                 }
 
             }
@@ -208,6 +273,180 @@ public class CustomersInstallment extends AppCompatActivity  {
         }
     }
 
+    //--------------------------------------------------------------------------------------------------
+    //For Bluetooth
+//--------------------------------------------------------------------------------------------------
+// close the connection to bluetooth printer.
+    void closeBT() throws IOException {
+        try {
+            stopWorker = true;
+            mmOutputStream.close();
+            mmInputStream.close();
+            mmSocket.close();
+//        myLabel.setText("Bluetooth Closed");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    void findBT() {
 
+        try {
+            mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+            if(mBluetoothAdapter == null) {
+//                myLabel.setText("No bluetooth adapter available");
+            }
+
+            if(!mBluetoothAdapter.isEnabled()) {
+                Intent enableBluetooth = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableBluetooth, 0);
+            }
+
+            Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
+
+            if(pairedDevices.size() > 0) {
+                for (BluetoothDevice device : pairedDevices) {
+
+                    // RPP300 is the name of the bluetooth printer device
+                    // we got this name from the list of paired devices
+                    if (device.getName().equals("MTP-3")) {
+                        mmDevice = device;
+                        break;
+                    }
+                }
+            }
+
+//            myLabel.setText("Bluetooth device found.");
+
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    // tries to open a connection to the bluetooth printer device
+    void openBT() throws IOException {
+        try {
+
+            // Standard SerialPortService ID
+            UUID uuid = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb");
+            mmSocket = mmDevice.createRfcommSocketToServiceRecord(uuid);
+            mmSocket.connect();
+            mmOutputStream = mmSocket.getOutputStream();
+            mmInputStream = mmSocket.getInputStream();
+
+            beginListenForData();
+
+//            myLabel.setText("Bluetooth Opened");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    /*
+     * after opening a connection to bluetooth printer device,
+     * we have to listen and check if a data were sent to be printed.
+     */
+    void beginListenForData() {
+        try {
+            final Handler handler = new Handler();
+
+            // this is the ASCII code for a newline character
+            final byte delimiter = 10;
+
+            stopWorker = false;
+            readBufferPosition = 0;
+            readBuffer = new byte[1024];
+
+            workerThread = new Thread(new Runnable() {
+                public void run() {
+
+                    while (!Thread.currentThread().isInterrupted() && !stopWorker) {
+
+                        try {
+
+                            int bytesAvailable = mmInputStream.available();
+
+                            if (bytesAvailable > 0) {
+
+                                byte[] packetBytes = new byte[bytesAvailable];
+                                mmInputStream.read(packetBytes);
+
+                                for (int i = 0; i < bytesAvailable; i++) {
+
+                                    byte b = packetBytes[i];
+                                    if (b == delimiter) {
+
+                                        byte[] encodedBytes = new byte[readBufferPosition];
+                                        System.arraycopy(
+                                                readBuffer, 0,
+                                                encodedBytes, 0,
+                                                encodedBytes.length
+                                        );
+
+                                        // specify US-ASCII encoding
+                                        final String data = new String(encodedBytes, "US-ASCII");
+                                        readBufferPosition = 0;
+
+                                        // tell the user data were sent to bluetooth printer device
+                                        handler.post(new Runnable() {
+                                            public void run() {
+//                                                myLabel.setText(data);
+                                            }
+                                        });
+
+                                    } else {
+                                        readBuffer[readBufferPosition++] = b;
+                                    }
+                                }
+                            }
+
+                        } catch (IOException ex) {
+                            stopWorker = true;
+                        }
+
+                    }
+                }
+            });
+
+            workerThread.start();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    // this will send text data to be printed by the bluetooth printer
+    void sendData(String BILL, final ProgressDialog o) throws IOException {
+        try {
+
+            // the text typed by the user
+            String msg ;
+            msg = BILL;
+            mmOutputStream.write(msg.getBytes());
+            //Added by chata
+//            closeBT();
+            Handler handler = new Handler();
+            handler.postDelayed(new Runnable() {
+                public void run() {
+                    // Actions to do after 3 seconds
+                    o.hide();
+                    CustomersInstallment.this.finish();
+                    try {
+                        closeBT();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }, 3000);
+
+            // tell the user data were sent
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+//--------------------------------------------------------------------------------------------------
+    //For bluetooth
+//--------------------------------------------------------------------------------------------------
 
     }
